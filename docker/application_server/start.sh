95fc27213f60e7cfd017bec02f77a5ebdaed925b
Arbër Kuçi <a.kuci@abidat.de>
2017-09-03 13:32:43 +0200

    Automating the deploy process with docker...

--- docker/application_server/start.sh
+++ docker/application_server/start.sh
@@ -10,7 +10,9 @@ asadmin start-domain domain1 && \
         asadmin --user $ADMIN_USER --passwordfile=Old_Password.txt change-admin-password && \
         asadmin --user $ADMIN_USER --passwordfile=New_Password.txt enable-secure-admin && \
 	asadmin stop-domain domain1 && \
-	asadmin start-domain domain1
+	asadmin start-domain domain1 && \
+	asadmin --user $ADMIN_USER --passwordfile=New_Password.txt deploy StoreApp-ear.ear	
+
 
 while true
 do
