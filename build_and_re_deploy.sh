90830c401f175eae8bf7768755b26b54c3f56fa0
Arbër Kuçi <arber.kuci2@gmail.com>
2017-09-28 19:25:53 +0200

    Executing docker commands as root user

--- build_and_re_deploy.sh
+++ build_and_re_deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo '<<<<<< - Stoping the containers -- if any is up and running - >>>>>>'
-docker stop shop-ee-mysql-db
-docker stop shop-ee-payara-full
+sudo docker stop shop-ee-mysql-db
+sudo docker stop shop-ee-payara-full
 
 echo '<<<<<< - Removing containers -- if any of them exists - >>>>>>'
-docker rm shop-ee-mysql-db
-docker rm shop-ee-payara-full
+sudo docker rm shop-ee-mysql-db
+sudo docker rm shop-ee-payara-full
 
 set -e
 
@@ -16,10 +16,10 @@ mvn clean install
 cp StoreApp-ear/target/StoreApp-ear.ear docker/application_server/
 
 echo '<<<<<< - Building docker images. - >>>>>'
-docker-compose build
+sudo docker-compose build
 
 echo '<<<<<< - Starting containers. - >>>>>>'
-docker-compose up -d
+sudo docker-compose up -d
 
 rm docker/application_server/StoreApp-ear.ear
 
